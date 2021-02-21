de86ce8d145f62e5b4e52d3680b860b747d7141b
Martin <mr.mmaurer+work@gmail.com>
2021-02-21 23:34:04 +0100

    Added dotenv and config vars

--- scraper/src/index.ts
+++ scraper/src/index.ts
@@ -1,9 +1,15 @@
+import dotenv from 'dotenv';
 import scrape from './scraping';
+
+dotenv.config();
+
+const password = process.env.PASSWORD;
+const url = process.env.URL;
 
 const scrapedPage = scrape();
 
 scrapedPage.then(async (page) => {
   // const html = await page.content();
 
-  console.log(page);
+  // console.log(page);
 });
