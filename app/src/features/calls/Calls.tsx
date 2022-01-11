6178dda3017f45de41fad9ebe35595ae8596bd6b
Martin <mr.mmaurer+work@gmail.com>
2022-01-11 20:52:18 +0000

    Added store update

--- app/src/features/calls/Calls.tsx
+++ app/src/features/calls/Calls.tsx
@@ -1,20 +1,30 @@
 import {ReactElement} from 'react';
 import { RootState } from '../../app/store';
+import { useSelector, useDispatch } from 'react-redux'
+import { addCall } from './callSlice';
+
 
 export default function Calls(): ReactElement  {
-  const x = 5;
+  const numberOfCalls = useSelector((state: RootState) => state.calls.calls.length)
+  const callList = useSelector((state: RootState) => state.calls.calls)
+  const dispatch = useDispatch()
 
   function handleClick(): void {
-    console.log('click');
+    const currentDate = Date.now();
+
+    dispatch(addCall(currentDate))
   }
 
   return (
     <div className='container'>
       <code>
-        {x}
+        {numberOfCalls}
+      </code>
+      <code>
+        <pre>{JSON.stringify(callList, null, 2)}</pre>
       </code>
       <button type="button" onClick={handleClick}>
-        Add call
+        Add a call
       </button>
     </div>
   )
