ad734b7f5c33b2d9c07babfa6a166c00c84c90b1
Martin <mr.mmaurer+work@gmail.com>
2022-01-11 20:52:47 +0000

    Cleanup

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -2,15 +2,11 @@ import React from 'react';
 // import { Counter } from './features/counter/Counter';
 import Calls from './features/calls/Calls';
 import './App.css';
-import dummyJSON from './data/dummy.json';
 
 function App() {
   return (
     <div className="App">
       <Calls />
-      <code>
-        <pre>{JSON.stringify(dummyJSON, null, 2)}</pre>
-      </code>
     </div>
   );
 }
