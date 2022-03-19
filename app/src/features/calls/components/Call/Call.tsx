1a110edb5361eda793c213a24a61c12c317b6142
Martin <mr.mmaurer+work@gmail.com>
2022-03-19 01:20:43 +0000

    Added π chart

--- app/src/features/calls/components/Call/Call.tsx
+++ app/src/features/calls/components/Call/Call.tsx
@@ -21,46 +21,69 @@ const Call: VFC<Readonly<Types.CallProps>> = ({ calls }) => {
 
   const [numberOfCodecs, _, codecStatisticRelative] = useCallStatistics(receiveCodecs);
 
-  const colours = ['red', 'green', 'blue', 'yellow', 'orange', 'deeppink']
-  const gradientSections = codecStatisticRelative.map(([_, percentage], index) => {
-    const colour = colours[index]; // todo wrap around
-    const word = `${colour} %0 ${percentage.toFixed(5)}%`
+  const colours = ['red', 'green', 'blue', 'yellow', 'orange', 'deeppink'];
+  const gradientSections = codecStatisticRelative.reduce(
+    (total, current, index) => {
+      const colour = colours[index]; // todo wrap around
+      const endValue = total.startValue + current[1];
 
-    return word
-  })
+      const section = `${colour} ${total.startValue.toFixed(5)}% ${endValue.toFixed(5)}%`;
 
-  console.log(gradientSections.join(', '))
+      const newTotal = {
+        startValue: total.startValue + current[1],
+        sections: total.sections.concat(section),
+      };
+
+      return newTotal;
+    },
+    {
+      startValue: 0,
+      sections: [] as string[],
+    }
+  );
+
+  const gradientsSectionsString = gradientSections.sections.join(', ');
 
   const pieChartStyle = {
     width: '250px',
     height: '250px',
-    background: "conic-gradient(red 0% 35%, green 0% 60%, blue 0% 100%)",
-    borderRadius: "50%"
+    background: `conic-gradient(${gradientsSectionsString})`,
+    borderRadius: '50%',
   };
 
   return (
     <div className="container">
-      <h2>Codecs statistics:</h2>
+      <h2>Codecs statistics</h2>
       <dl>
-        {codecStatisticRelative.map(([key, value]) => (
+        {codecStatisticRelative.map(([key, value], index) => (
           <Fragment key={key}>
             <dt>{key}:</dt>
-            <dt>{value.toFixed(2)}%</dt>
+            <dt>
+              <span>{value.toFixed(2)}%</span>
+              <span
+                style={{
+                  display: 'inline-block',
+                  marginLeft: '1rem',
+                  width: '1ch',
+                  height: '1ch',
+                  background: `${colours[index]}`,
+                }}
+              ></span>
+            </dt>
           </Fragment>
         ))}
       </dl>
+      <h2>Pie chart</h2>
       <div className="pie-chart" style={pieChartStyle} />
-      <div>
-        <h2>{numberOfCodecs} entries for the day</h2>
-        <code>
-          {Boolean(entriesForDay) &&
-            entriesForDay.map((group, index) => (
-              <Fragment key={index}>
-                <pre>{JSON.stringify(group, null, 4)}</pre>
-              </Fragment>
-            ))}
-        </code>
-      </div>
+      <h2>{numberOfCodecs} entries for the day</h2>
+      <code>
+        {Boolean(entriesForDay) &&
+          entriesForDay.map((group, index) => (
+            <Fragment key={index}>
+              <pre>{JSON.stringify(group, null, 4)}</pre>
+            </Fragment>
+          ))}
+      </code>
     </div>
   );
 };
--- app/src/features/calls/hooks/useCallStatistics/useCallStatistics.types.ts
+++ app/src/features/calls/hooks/useCallStatistics/useCallStatistics.types.ts
@@ -2,4 +2,4 @@ export type Codec = string | null;
 
 export type StatisticsAbsolute = Record<Extract<Codec, string>, number>;
 
-export type StatisticsRelative = [codecName:  Extract<Codec, string>, percentage: number][];
+export type StatisticsRelative = [codecName: Extract<Codec, string>, percentage: number][];
