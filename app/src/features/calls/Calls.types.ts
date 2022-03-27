1a884fb172760d1fff1b365c06c75a8b8b92b0a6
Martin <mr.mmaurer+work@gmail.com>
2022-03-27 20:11:08 +0100

    Improved typing for Codec and added more code types to dummy data

--- app/src/features/calls/Calls.types.ts
+++ app/src/features/calls/Calls.types.ts
@@ -1,3 +1,1 @@
-export type CallsProps = {
-};
-
+export type CallsProps = {};
--- app/src/features/calls/callsApi.ts
+++ app/src/features/calls/callsApi.ts
@@ -1,5 +1,17 @@
 import callJSON from '../../data/dummy.json';
 
+const codecsValues = ['G.711', 'G.722', 'G.726', 'G.729'] as const;
+type Codec = typeof codecsValues[number];
+
+// Codec type guard
+function isCodec(codecString: string | null): codecString is Codec {
+  if (!codecString) {
+    return false;
+  }
+
+  return codecsValues.includes(codecString as Codec);
+}
+
 type CallStringlyTyped = {
   dateTime: string;
   codecs: {
@@ -8,8 +20,13 @@ type CallStringlyTyped = {
   };
 };
 
-export type Call = Omit<CallStringlyTyped, 'dateTime'> & {
+// export type Call = Omit<CallStringlyTyped, 'codecs'> & {
+export type Call = {
   dateTime: ReturnType<typeof Date['now']>;
+  codecs: {
+    send: Codec | null;
+    receive: Codec | null;
+  };
 };
 
 export function getEntry(): Promise<ReturnType<typeof Date['now']>> {
@@ -23,12 +40,16 @@ export function getCallList(): Promise<Call[]> {
     const callListRaw: CallStringlyTyped[] = callJSON;
 
     const callList: Call[] = callListRaw.map((call) => {
-      const { dateTime } = call;
+      const { dateTime, codecs } = call;
+      const { send, receive } = codecs;
       const dateTimeParsed = Date.parse(dateTime);
 
       return {
-        ...call,
         dateTime: dateTimeParsed,
+        codecs: {
+          send: isCodec(send) ? send : null,
+          receive: isCodec(receive) ? receive : null,
+        },
       };
     });
 
