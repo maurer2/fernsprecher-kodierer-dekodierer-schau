e594048e8ffbb8ebd9ee568550b19c13fc1f79f7
Martin <mr.mmaurer+work@gmail.com>
2022-03-29 23:51:52 +0100

    Added dynamic leading zero insertion

--- app/src/features/calls/components/Statistics/Statistics.tsx
+++ app/src/features/calls/components/Statistics/Statistics.tsx
@@ -4,13 +4,32 @@ import * as Types from './Statistics.types';
 import { colourCodecMap } from '../PieChart/constants';
 
 const Statistics: VFC<Readonly<Types.StatisticsProps>> = ({ codecStatistics }) => {
+  const digitsAll = codecStatistics.reduce(
+    (total, current) => {
+      // const percentageDigits = current[1].toFixed(2).toString().length;
+      const countDigits = current[2].toString().length;
+
+      // total['percentage'] = total['percentage'].concat(percentageDigits);
+      total['count'] = total['count'].concat(countDigits);
+
+      return total;
+    },
+    {
+      percentage: [] as number[],
+      count: [] as number[],
+    }
+  );
+
+  // const numberOfDigitsForPercentage = Math.max(...digitsAll.percentage);
+  const numberOfDigitsForCount = Math.max(...digitsAll.count);
+
   return (
     <dl>
       {codecStatistics.map(([name, percentage, count]) => (
         <Fragment key={name}>
           <dt>{name}:</dt>
           <dd>{percentage.toFixed(2)}%</dd>
-          <dd>{count}</dd>
+          <dd>{String(count).padStart(numberOfDigitsForCount, '0')}</dd>
           <dd>
             <span
               style={{
