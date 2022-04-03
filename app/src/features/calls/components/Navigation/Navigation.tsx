3397e760ff333e46a5873a784fe3e896242d617a
Martin <mr.mmaurer+work@gmail.com>
2022-04-03 01:21:24 +0100

    Added active styles to menu

--- app/src/features/calls/components/Navigation/Navigation.tsx
+++ app/src/features/calls/components/Navigation/Navigation.tsx
@@ -1,27 +1,33 @@
-import { ReactElement, Fragment, VFC } from 'react';
+import { ReactElement, VFC } from 'react';
 import { useParams } from 'react-router-dom';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
+import CSS from 'csstype';
 
-import * as Types from './Navigation.types'
+import * as Types from './Navigation.types';
 
-const Navigation: VFC<Readonly<Types.NavigationProps>> = ({daysWithCalls}): ReactElement => {
-  const activeDate = false;
+const Navigation: VFC<Readonly<Types.NavigationProps>> = ({ daysWithCalls }): ReactElement => {
   const { day = '' } = useParams();
 
+  const activeStyles: CSS.Properties = {
+    background: '#00e',
+    color: '#fff',
+    borderColor: '#00e',
+  };
+
   return (
-    <nav className='nav'>
-      {daysWithCalls.map((day) => (
-        <Fragment key={day}>
-          <Link
-            to={`/calls/${day}`}
-            className='nav-link'
-          >
-            {day}
-          </Link>
-        </Fragment>
+    <nav className="nav">
+      {daysWithCalls.map((dayEntry) => (
+        <NavLink
+          key={dayEntry}
+          to={`/calls/${dayEntry}`}
+          className="nav-link"
+          style={dayEntry === day ? activeStyles : {}}
+        >
+          {dayEntry}
+        </NavLink>
       ))}
     </nav>
   );
-}
+};
 
-export default Navigation
+export default Navigation;
