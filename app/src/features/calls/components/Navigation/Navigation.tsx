f49ab8d3398f6d7b25000847d6bb742c6ec5b56b
Martin <mr.mmaurer+work@gmail.com>
2022-05-22 15:39:00 +0100

    Added scrolling to active day when document is resized

--- app/src/features/calls/components/Navigation/Navigation.tsx
+++ app/src/features/calls/components/Navigation/Navigation.tsx
@@ -1,5 +1,12 @@
 import React, {
-  ReactElement, FC, MouseEvent, useEffect, useMemo, RefObject, useCallback,
+  ReactElement,
+  FC,
+  MouseEvent,
+  useEffect,
+  useMemo,
+  RefObject,
+  useCallback,
+  useRef,
 } from 'react';
 
 import * as Types from './Navigation.types';
@@ -9,6 +16,7 @@ const Navigation: FC<Readonly<Types.NavigationProps>> = ({
   daysWithCalls,
   currentDay,
 }): ReactElement => {
+  const NavigationElement = useRef<HTMLElement | null>(null);
   const linkElements = useMemo<RefObject<HTMLAnchorElement>[]>(
     () => Array.from({ length: daysWithCalls.length }, () => React.createRef()),
     [daysWithCalls],
@@ -18,25 +26,46 @@ const Navigation: FC<Readonly<Types.NavigationProps>> = ({
 
   function gotoNextDay(event: MouseEvent<HTMLButtonElement>): void {}
 
-  const scrollToDay = useCallback((domElement: RefObject<HTMLElement>) => {
-    domElement.current?.scrollIntoView({
+  const scrollToActiveLinkElement = useCallback(() => {
+    const activeLinkElement = linkElements.find((element) => element.current?.classList.contains('active'));
+
+    activeLinkElement?.current?.scrollIntoView({
       behavior: 'smooth',
       block: 'nearest',
     });
-  }, []);
+  }, [linkElements]);
 
+  // scroll to active link element when viewport changes
   useEffect(() => {
-    const activeLinkElement = linkElements.find((element) => element.current?.classList.contains('active'));
+    let linkListRef: HTMLElement | null = null;
 
-    if (!currentDay || !activeLinkElement?.current) {
+    const NavigationElementResizeObserver = new ResizeObserver(() => {
+      scrollToActiveLinkElement();
+    });
+
+    if (NavigationElement.current) {
+      NavigationElementResizeObserver.observe(NavigationElement.current);
+      linkListRef = NavigationElement.current;
+    }
+
+    return () => {
+      if (linkListRef) {
+        NavigationElementResizeObserver.unobserve(linkListRef);
+      }
+    };
+  }, [scrollToActiveLinkElement]);
+
+  // scroll to active link element when current day changes
+  useEffect(() => {
+    if (!currentDay) {
       return;
     }
 
-    scrollToDay(activeLinkElement);
-  }, [currentDay, linkElements, scrollToDay]);
+    scrollToActiveLinkElement();
+  }, [currentDay, scrollToActiveLinkElement]);
 
   return (
-    <Styles.Navigation>
+    <Styles.Navigation ref={NavigationElement}>
       <Styles.LinkList>
         {daysWithCalls.map((day, index) => (
           <Styles.LinkListEntry key={day.iso}>
