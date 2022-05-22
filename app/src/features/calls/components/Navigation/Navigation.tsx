66042c751732a0f2a46405f428f469c8362233fa
Martin <mr.mmaurer+work@gmail.com>
2022-05-22 01:07:51 +0100

    Added scrolling to current day on load

--- app/src/features/calls/components/Navigation/Navigation.tsx
+++ app/src/features/calls/components/Navigation/Navigation.tsx
@@ -1,4 +1,6 @@
-import { ReactElement, FC, MouseEvent } from 'react';
+import React, {
+  ReactElement, FC, MouseEvent, useEffect, useMemo, RefObject, useCallback,
+} from 'react';
 
 import * as Types from './Navigation.types';
 import * as Styles from './Navigation.styles';
@@ -7,19 +9,41 @@ const Navigation: FC<Readonly<Types.NavigationProps>> = ({
   daysWithCalls,
   currentDay,
 }): ReactElement => {
+  const linkElements = useMemo<RefObject<HTMLAnchorElement>[]>(
+    () => Array.from({ length: daysWithCalls.length }, () => React.createRef()),
+    [daysWithCalls],
+  );
+
   function gotoPrevDay(event: MouseEvent<HTMLButtonElement>): void {}
 
   function gotoNextDay(event: MouseEvent<HTMLButtonElement>): void {}
 
+  const scrollToDay = useCallback((domElement: RefObject<HTMLElement>) => {
+    domElement.current?.scrollIntoView({
+      behavior: 'smooth',
+      block: 'nearest',
+    });
+  }, []);
+
+  useEffect(() => {
+    const activeLinkElement = linkElements.find((element) => element.current?.classList.contains('active'));
+
+    if (!currentDay || !activeLinkElement?.current) {
+      return;
+    }
+
+    scrollToDay(activeLinkElement);
+  }, [currentDay, linkElements, scrollToDay]);
+
   return (
     <Styles.Navigation>
       <Styles.LinkList>
-        {daysWithCalls.map((day) => (
+        {daysWithCalls.map((day, index) => (
           <Styles.LinkListEntry key={day.iso}>
             <Styles.Link
               to={`/calls/${day.iso}`}
-              className="nav-link"
               status={day.iso === currentDay ? 'active' : 'default'}
+              ref={linkElements[index]}
             >
               {day.user}
             </Styles.Link>
@@ -29,12 +53,14 @@ const Navigation: FC<Readonly<Types.NavigationProps>> = ({
       <Styles.NavButton
         type="button"
         onClick={(event: MouseEvent<HTMLButtonElement>) => gotoPrevDay(event)}
+        disabled
       >
         Previous day
       </Styles.NavButton>
       <Styles.NavButton
         type="button"
-        onClick={(event: MouseEvent<HTMLButtonElement>) => gotoPrevDay(event)}
+        onClick={(event: MouseEvent<HTMLButtonElement>) => gotoNextDay(event)}
+        disabled
       >
         Next day
       </Styles.NavButton>
