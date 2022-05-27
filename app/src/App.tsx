dc99c8a26ed430e2fe54ab0bd5b46d29c22785b8
Martin <mr.mmaurer+work@gmail.com>
2022-05-27 22:08:42 +0100

    Removed home and made calls homepage

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 import Calls from './features/calls';
-import Home from './features/home/index';
 
 import 'modern-normalize';
 
@@ -18,16 +17,12 @@ const App = () => {
       <Router>
         <Routes>
           <Route
-            path="/calls"
-            element={<Calls />}
-          />
-          <Route
             path="/calls/:day"
             element={<Calls />}
           />
           <Route
             path="*"
-            element={<Home />}
+            element={<Calls />}
           />
         </Routes>
       </Router>
