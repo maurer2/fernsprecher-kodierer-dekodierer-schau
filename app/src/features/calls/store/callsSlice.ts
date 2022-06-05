b8c3cec377ad6c18403805ff15f658132f0e346c
Martin <mr.mmaurer+work@gmail.com>
2022-06-05 13:43:41 +0200

    Replaced lodash groupby with grouping via reduce and added readonlyarray to prevent mutating via sort

--- app/src/features/calls/store/callsSlice.ts
+++ app/src/features/calls/store/callsSlice.ts
@@ -1,7 +1,4 @@
-import {
-  createSlice, PayloadAction, createAsyncThunk, current,
-} from '@reduxjs/toolkit';
-import { groupBy } from 'lodash-es';
+import { createSlice, PayloadAction, createAsyncThunk } from '@reduxjs/toolkit';
 import { getCallList } from './callsApi';
 import type {
   CallsSliceState, CallWithDates, CallMap, Day,
@@ -44,36 +41,46 @@ export const callsSlice = createSlice({
       .addCase(getCalls.pending, (state) => {
         state.isLoading = true;
       })
-      .addCase(getCalls.fulfilled, (state, action: PayloadAction<CallWithDates[]>) => {
-        const { payload } = action;
-        const callsSorted: CallWithDates[] = [...payload].sort((a, z) => a.dateTime - z.dateTime);
+      .addCase(
+        getCalls.fulfilled,
+        (state, { payload }: PayloadAction<ReadonlyArray<CallWithDates>>) => {
+          const callsSorted: CallWithDates[] = [...payload].sort((a, z) => a.dateTime - z.dateTime);
 
-        // grouping
-        const callsGroupedByIsoDate: Record<Day, CallWithDates[]> = groupBy(
-          callsSorted,
-          'dates.iso',
-        );
-        const callsGrouped: CallMap = Object.fromEntries(
-          Object.entries(callsGroupedByIsoDate).map(([day, entries]) => {
-            const entriesWithoutDate = entries.map(({ dateTime, codecs }) => ({
-              dateTime,
-              codecs,
-            }));
-            const entriesFormatted: CallMap[number] = {
-              dayDates: entries[0].dates,
-              entries: entriesWithoutDate,
-            };
+          const callsGroupedByIsoDate: Record<Day, CallWithDates[]> = callsSorted.reduce(
+            (total, current) => {
+              if (!(current.dates.iso in total)) {
+                total[current.dates.iso] = [];
+              }
 
-            return [day, entriesFormatted];
-          }),
-        );
+              total[current.dates.iso].push(current);
 
-        const mostRecentDay = callsSorted?.at(-1)?.dates ?? null;
+              return total;
+            },
+            {} as Record<Day, CallWithDates[]>,
+          );
 
-        state.isLoading = false;
-        state.callList = callsGrouped;
-        state.mostRecentDay = mostRecentDay;
-      })
+          const callsGrouped: CallMap = Object.fromEntries(
+            Object.entries(callsGroupedByIsoDate).map(([day, entries]) => {
+              const entriesWithoutDate = entries.map(({ dateTime, codecs }) => ({
+                dateTime,
+                codecs,
+              }));
+              const entriesFormatted: CallMap[number] = {
+                dayDates: entries[0].dates,
+                entries: entriesWithoutDate,
+              };
+
+              return [day, entriesFormatted];
+            }),
+          );
+
+          const mostRecentDay = callsSorted?.at(-1)?.dates ?? null;
+
+          state.isLoading = false;
+          state.callList = callsGrouped;
+          state.mostRecentDay = mostRecentDay;
+        },
+      )
       .addCase(getCalls.rejected, (state) => {
         state.isLoading = false;
         state.callList = null;
