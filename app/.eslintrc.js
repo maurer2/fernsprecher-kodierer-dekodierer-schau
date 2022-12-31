974159b0e8868a9e01341479ada292cceb22faea
Martin <mr.mmaurer+work@gmail.com>
2022-12-31 01:52:13 +0100

    Fixed TS type warnings for jest and added bundle analyzer

--- app/.eslintrc.js
+++ app/.eslintrc.js
@@ -3,6 +3,7 @@ module.exports = {
     es2021: true,
     node: true,
     browser: true,
+    jest: true,
   },
   extends: [
     'eslint:recommended',
--- app/vite.config.ts
+++ app/vite.config.ts
@@ -1,11 +1,28 @@
 import react from '@vitejs/plugin-react';
 import { vanillaExtractPlugin } from '@vanilla-extract/vite-plugin';
+import checker from 'vite-plugin-checker';
+import { visualizer } from 'rollup-plugin-visualizer';
+
+import type { PluginOption } from 'vite';
 
 export default {
+  server: {
+    open: false,
+  },
   plugins: [
     react(),
     vanillaExtractPlugin({
       identifiers: 'debug',
     }),
+    checker({
+      typescript: true,
+    }),
+    visualizer({
+      template: 'treemap',
+      filename: 'statistics/statistics.html',
+      gzipSize: true,
+      brotliSize: true,
+      open: true,
+    }) satisfies PluginOption,
   ],
 };
