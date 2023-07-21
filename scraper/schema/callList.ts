d271e7ee79696781e4f32c8eafedc3265d92afd8
Martin <mr.mmaurer+work@gmail.com>
2023-07-21 23:31:41 +0200

    Simplified callList schema

--- scraper/schema/callList.ts
+++ scraper/schema/callList.ts
@@ -1,27 +1,21 @@
 import { z } from 'zod';
 
-export const callSchema = z
-  .object({
-    // dateTime
-    dateTime: z.string().datetime().nullable(),
-    // codecs
-    codecs: z
-      .object({
-        send: z.union([
-          z.literal('G.711'),
-          z.literal('G.722-HD'),
-          z.literal('G.726'),
-          z.literal('G.729'),
-        ]).array().nullable(),
-        receive: z.union([
-          z.literal('G.711'),
-          z.literal('G.722-HD'),
-          z.literal('G.726'),
-          z.literal('G.729'),
-        ]).array().nullable(),
-      })
-      .strict(),
+const codecNames =  z.union([
+  z.literal('G.711'),
+  // z.literal('G.722'),
+  z.literal('G.722-HD'),
+  z.literal('G.726'),
+  z.literal('G.729'),
+])
+
+export const callSchema = z.object({
+  dateTime: z.string().datetime().nullable(),
+  codecs: z.object({
+    send: z.array(codecNames).nullable(),
+    receive: z.array(codecNames).nullable(),
   })
-  .strict();
+  .strict(),
+})
+.strict();
 
 export const callListSchema = z.array(callSchema);
